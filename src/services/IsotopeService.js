389ea05fbe50c5fef5c36774c9d0d409d47e2d58
AR2147 <131007645+AR2147@users.noreply.github.com>
2025-05-28 16:46:23 +0300

    FRONT-12 isotopeChange

--- src/services/IsotopeService.js
+++ src/services/IsotopeService.js
@@ -2,14 +2,30 @@ import axios from "axios";
 
 export default {
     sendGetActiveIsotopesRequest() {
-        return axios.get('/active-isotopes')
+        return axios.get('/isotope/active-isotopes')
     },
 
     sendGetIsotopeRequest() {
-        return axios.get('/isotopes')
+        return axios.get('/isotope/isotopes')
     },
 
     sendPostIsotopeRequest(isotopeData) {
-        return axios.post('/isotopes', isotopeData)
+        return axios.post('/isotope/isotopes', isotopeData)
     },
-};
+
+    sendUpdateIsotopeStatusRequest(id, isotopeStatus) {
+        return axios.patch('/isotope/' + id + '/isotope-status', null,
+            {
+                params: {
+                    status: isotopeStatus
+                }
+            }
+        )
+    },
+    sendUpdateIsotopeRequest(id, isotopeUpdate) {
+        return axios.patch(`/isotope/isotopes/${id}`, isotopeUpdate)
+    },
+    sendDeleteIsotopeRequest(id) {
+        return axios.delete(`/isotope/${id}`)
+    }
+}
